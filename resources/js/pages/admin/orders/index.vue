7113755b7c281a43de6e85f47ec0036068b985b8
Jersey276 <tristan-lefevre@hotmail.fr>
2025-08-08 13:03:36 +0200

    add template for order table

---
+++ resources/js/pages/admin/orders/index.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+import DataTable from '@/components/DataTable.vue';
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Dashboard',
+        href: route('admin.dashboard'),
+    },
+    {
+        title: 'Commande',
+        href: route('admin.orders.index'),
+    },
+];
+
+</script>
+<template>
+    <Head title="Categories" />
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <DataTable :api-url="route('api.orders.index')" :columns="[
+                { label: 'ID', field: 'id' },
+                { label: 'Date', field: 'created_at' },
+                { label: 'Total', field: 'total' },
+                { label: 'Statut', field: 'status' },
+                { label: 'Client', field: 'client.name'}
+            ]" :actionButtons="[
+                {
+                    label: 'Voir',
+                    action: (item: any) => {
+                        console.log('Voir commande', item);
+                    }
+                }
+            ]" />
+    </AppLayout>
+</template>
