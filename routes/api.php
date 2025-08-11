6f0b2c82cbd32dfb90b0cc356005e5c1a8695938
Jersey276 <tristan-lefevre@hotmail.fr>
2025-08-11 16:14:16 +0200

    add sanctum in api routes

--- routes/api.php
+++ routes/api.php
@@ -9,34 +9,35 @@ use App\Http\Controllers\ShippingMethodController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/users', [UserController::class, 'apiIndex'])->name('api.users.index');
-Route::get('/users/{user}', [UserController::class, 'apiView'])->name('api.users.view');
-Route::controller(ProductController::class)->prefix('products')->group(function () {
-    Route::get('/', 'apiIndex')->name('api.products.index');
-    Route::get('/{product}', 'apiView')->name('api.products.view');
-    Route::post('/', 'apiStore')->name('api.products.store');
-    Route::put('/{product}', 'apiUpdate')->name('api.products.update');
-});
-Route::controller(ShippingMethodController::class)->prefix('shipping')->group(function () {
-    Route::get('/', 'apiIndex')->name('api.shipping.index');
-    Route::get('/{shippingMethod}', 'apiView')->name('api.shipping.view');
-    Route::post('/', 'apiStore')->name('api.shipping.store');
-    Route::put('/{shippingMethod}', 'apiUpdate')->name('api.shipping.update');
-});
-Route::get('/addresses', [AddressController::class, 'apiIndex'])->name('api.addresses.index');
-Route::get('/orders', [OrderController::class, 'apiIndex'])->name('api.orders.index');
-Route::prefix('categories')->group(function () {
-    Route::controller(CategoryController::class)->group(function () {
-        Route::get('/', 'apiIndex')->name('api.categories.index');
-        Route::get('/{category}', 'apiView')->name('api.categories.view');
-        Route::post('/', 'apiStore')->name('api.categories.store');
-        Route::put('/{category}', 'apiUpdate')->name('api.categories.update');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/users', [UserController::class, 'apiIndex'])->name('api.users.index');
+    Route::get('/users/{user}', [UserController::class, 'apiView'])->name('api.users.view');
+    Route::controller(ProductController::class)->prefix('products')->group(function () {
+        Route::get('/', 'apiIndex')->name('api.products.index');
+        Route::get('/{product}', 'apiView')->name('api.products.view');
+        Route::post('/', 'apiStore')->name('api.products.store');
+        Route::put('/{product}', 'apiUpdate')->name('api.products.update');
+    });
+    Route::controller(ShippingMethodController::class)->prefix('shipping')->group(function () {
+        Route::get('/', 'apiIndex')->name('api.shipping.index');
+        Route::get('/{shippingMethod}', 'apiView')->name('api.shipping.view');
+        Route::post('/', 'apiStore')->name('api.shipping.store');
+        Route::put('/{shippingMethod}', 'apiUpdate')->name('api.shipping.update');
+    });
+    Route::get('/addresses', [AddressController::class, 'apiIndex'])->name('api.addresses.index');
+    Route::get('/orders', [OrderController::class, 'apiIndex'])->name('api.orders.index');
+    Route::prefix('categories')->group(function () {
+        Route::controller(CategoryController::class)->group(function () {
+            Route::get('/', 'apiIndex')->name('api.categories.index');
+            Route::get('/{category}', 'apiView')->name('api.categories.view');
+            Route::post('/', 'apiStore')->name('api.categories.store');
+            Route::put('/{category}', 'apiUpdate')->name('api.categories.update');
+        });
+    });
+    Route::controller(CartController::class)->group(function () {
+        Route::get('/cart', 'index')->name('api.cart.index');
+        Route::post('/cart/add', 'add')->name('api.cart.add');
+        Route::put('/cart/update', 'update')->name('api.cart.update');
+        Route::delete('/cart/remove/{product}', 'remove')->name('api.cart.remove');
     });
 });
-Route::controller(CartController::class)->group(function () {
-    Route::get('/cart', 'index')->name('api.cart.index');
-    Route::post('/cart/add', 'add')->name('api.cart.add');
-    Route::put('/cart/update', 'update')->name('api.cart.update');
-    Route::delete('/cart/remove/{product}', 'remove')->name('api.cart.remove');
-});
-
